499d330048feab23a9e464bc66c96e87774c2b95
Haresh Kotkar <75067419+harshkotkar5557@users.noreply.github.com>
2021-09-11 00:13:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const btnTranslator = document.querySelector("#btn-translate");
+const txtInput = document.querySelector("#txt-input");
+const outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/klingon.json";
+
+getTranslationURL = (input) => {
+    return serverURL + "?" + "text=" + input
+}
+
+errorHandler = (error) => {
+    console.log("error ocuured", error);
+    alert("something went wrong : try later");
+}
+
+clickHandler = () => {
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+
+        .then(response => response.json())
+        .then(json => {
+
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        //error handling
+        .catch(errorHandler)
+}
+
+btnTranslator.addEventListener("click", clickHandler)
